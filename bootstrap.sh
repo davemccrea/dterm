576cbb499d65ee05d8d3c53725b7d813fd457545
David McCrea <github@dmccrea.me>
2023-11-25 00:03:20 +0200

    build nodejs in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,6 +22,13 @@ asdf reshim erlang
 asdf global elixir latest
 asdf global erlang latest
 
+# TODO: move to Containerfile
+# Due to how the nodejs asdf plugin is implemented
+# the install script errors when run in Containerfile
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs 20.10.0
+asdf global nodejs 20.10.0
+
 # Setup Phoenix
 mix local.hex --force
 mix archive.install hex phx_new --force
