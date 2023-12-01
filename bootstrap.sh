6d355ec5fab2c6e86ac7d73ee23095957e1a2bd7
David McCrea <git@dmccrea.me>
2023-12-01 23:40:29 +0200

    install livebook on container init

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,12 +26,14 @@ sudo chown -R $USER $HOME/.asdf
 asdf global elixir latest
 asdf global erlang latest
 asdf global nodejs latest
-asdf reshim elixir
-asdf reshim erlang
-asdf reshim nodejs
 
 # Setup Phoenix
 # We want to use the .tool-versions in the container home dir not the host home directory
 cd $HOME 
 mix local.hex --force
 mix archive.install hex phx_new --force
+
+# Setup Livebook
+mix do local.rebar --force, local.hex --force
+mix escript.install hex livebook
+asdf reshim elixir
