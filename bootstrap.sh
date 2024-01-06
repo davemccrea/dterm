46dcb698f6dd3857ad47bf9ae00f1cd0a66f64bf
David McCrea <git@dmccrea.me>
2024-01-07 00:55:19 +0200

    use non-interactive when installing livebook

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,5 +38,4 @@ mix archive.install hex phx_new --force
 
 # Setup Livebook
 mix do local.rebar --force, local.hex --force
-mix escript.install hex livebook
-
+mix escript.install hex livebook --force
